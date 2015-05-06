3de0ee2c8868afc77c59ee48ebb137b6d5a3dda3
Ivikgh <andoidxx@gmail.com>
2015-05-06 19:30:01 +0300

    Contact page dublicated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
+
+  root 'pages#contact'
+  #get 'pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
