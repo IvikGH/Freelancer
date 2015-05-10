067a7b672e541d35007ae3b6a540797fc09c8816
Ivikgh <andoidxx@gmail.com>
2015-05-10 17:27:19 +0300

    Configured production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'andoidxx',
+    password: 'D2c47809'
+  }
+
 end
